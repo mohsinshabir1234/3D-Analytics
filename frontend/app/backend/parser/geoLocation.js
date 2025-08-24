eab6828a404269db5e75c7218458a162732f7bf5
mac <mac@macs-MacBook-Pro.local>
2025-08-24 13:12:19 +0400

    added geo-ip-lite library to retrieve location from IP

---
+++ frontend/app/backend/parser/geoLocation.js
@@ -0,0 +1,10 @@
+import geoip from 'geoip-lite';
+
+
+export default function geo_lookup(ip_address){
+    //remember if ip is private, it will show null
+var geo = geoip.lookup(ip_address);
+
+console.log("This is ",geo)
+return geo.country
+}
--- frontend/app/backend/parser/parser.js
+++ frontend/app/backend/parser/parser.js
@@ -2,6 +2,12 @@ import fs from "fs";
 import readline from "readline";
 import { coordinates_data } from "./clustering.js";
 import clustering from "./clustering.js";
+import geo_lookup from "./geoLocation.js";
+
+export let infoCount=0
+export let warningCount=0
+export let errorCount=0
+
 async function parseLogFile(filePath) {
   const fileStream = fs.createReadStream(filePath);
   const rl = readline.createInterface({
@@ -24,10 +30,27 @@ async function parseLogFile(filePath) {
       console.log("----");
       rawData.push(parsed.coordinates);
 
+      switch(parsed.level){
+      case "INFO":
+        infoCount++;
+        break;
+      case "ERROR":
+        errorCount++;
+        break;
+      case "WARNING":
+        warningCount++;
+        break;
+      default:
+        console.log("Unknown log level detected ")
+      }
+    geo_lookup(parsed.ip)
+
     } catch (err) {
       console.error("Failed to parse line:", line, err.message);
     }
   }
+      console.log("These are level counts",infoCount,errorCount,warningCount)
+
     coordinates_data.length =0;
     for (const p of rawData) {
       coordinates_data.push([p.x, p.y, p.z]);
@@ -36,7 +59,9 @@ async function parseLogFile(filePath) {
       console.log(rawData)
       // console.log("This is coordinates data ",coordinates_data)
     clustering()
+    
 
 }
+
 
 export default parseLogFile;
