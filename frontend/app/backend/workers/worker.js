b9ccec6df845a20bc36120da270197f961de5552
mac <mac@macs-MacBook-Pro.local>
2025-08-25 18:00:45 +0400

    used await before parselogfile and replaced logfilwithPino with it

--- frontend/app/backend/workers/worker.js
+++ frontend/app/backend/workers/worker.js
@@ -1,17 +1,11 @@
 import {Worker} from 'bullmq';
 import IORedis from 'ioredis';
-import logFileWithPino from '../parser/parser.js';
-import wss from ''
-import { createClient } from '@supabase/supabase-js';
-import { createClient as createRedisClient } from 'redis';
-
-const supabase = createClient(process.env.NEXT_SUPABASE_URL, process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY);
-const redis = createRedisClient({ url: process.env.REDIS_URL });
+import parseLogFile from '../parser/parser.js';
 
 const connection = new IORedis({maxRetriesPerRequest:null});
 
 const logFileWorker = new Worker("logFileForParsing",async job =>{
-  logFileWithPino(job.data.location)
+   await parseLogFile(job.data.location)
     await job.updateProgress(100);
     console.log("Job Done")
     return "Job done"
